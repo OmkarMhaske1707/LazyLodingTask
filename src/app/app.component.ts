52cf4a1ba742e311e68c7f54d6506cff8f46a1ba
OmkarM <ommkarmhaske1707@gmail.com>
2023-11-29 16:05:29 +0530

    constructor and ngOnInit created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  constructor(){
+    console.log("feature-2 branch,");
+
+  }
+  ngOnInit(): void {
+  }
+
+  
   title = 'lazyAssignment';
 
   msg:any = "devlop branch,Angular Devloper....";
